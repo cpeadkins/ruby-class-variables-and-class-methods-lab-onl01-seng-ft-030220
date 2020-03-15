e8f7d12c3f2f19735ea05eac5831c7ae32c0cb6a
Charles Adkins <cpeadkins@gmail.com>
2020-03-15 03:37:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,23 +1,24 @@
 class Song
   attr_accessor :name,:artist,:genre
   @@count = 0
-  # @@artist_count = 0
-  # @@genre_count = 0
+  @@artists = []
+  @@genres = []
+  @@genre_count = {}
 
   def initialize(name,artist,genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count += 1
-    # @@artist_count += 1
-    # @@genre_count += 1
+    @@artists << @artist
+    @@genres << @genre
   end
 
   def self.count
     @@count
   end
   
-  def artists
+  def self.artists #self.artists= ?
     @@artists = artists
   end
   
