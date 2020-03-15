8baa162e50a9953729fa8c0670a7151f2ed8c5c4
Charles Adkins <cpeadkins@gmail.com>
2020-03-15 03:38:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,5 +23,44 @@ class Song
   end
   
 
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres=
+    @genre = genres
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    @@genre_count = {}
+  end
+
+  def self.genre_count
+    g_count = {}
+    @@genres.each do | genre |
+      if g_count[genre]
+        g_count[genre] += 1
+      else
+        g_count[genre] = 1
+      end
+    end
+    g_count
+  end
+
+  def self.artist_count
+    a_count = {}
+    @@artists.each do | artist |
+      if a_count[artist]
+        a_count[artist] += 1
+      else
+        a_count[artist] = 1
+      end
+    end
+    a_count
+  end
 
 end
